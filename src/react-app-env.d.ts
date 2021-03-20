8f05a40fc5744c5d4add4ca188bcef49dadf27d1
WCihan <chntklc@gmail.com>
2021-03-20 16:34:02 +0300

    added navbar and mobile navbar styles

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare module '*.svg' {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	export const ReactComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+	const src: string;
+	export default src;
+}
